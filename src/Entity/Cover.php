c1a6fd31cfc5b36b69d7391ffcef534d804a35d6
Alexis Leroy <a.leroy@mentalworks.fr>
2020-04-14 22:31:19 +0200

    start update list algorithm

---
+++ src/Entity/Cover.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Cover
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="covers")
+ */
+class Cover
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
+    private int $igdbId;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private string $imageId;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
+    private int $height;
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
+    private int $width;
+
+    /**
+     * @var Game
+     * @ORM\OneToOne(targetEntity="Game")
+     */
+    private Game $game;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getIgdbId(): int
+    {
+        return $this->igdbId;
+    }
+
+    public function getImageId(): string
+    {
+        return $this->imageId;
+    }
+
+    public function setImageId(string $imageId): self
+    {
+        $this->imageId = $imageId;
+
+        return $this;
+    }
+
+    public function getHeight(): int
+    {
+        return $this->height;
+    }
+
+    public function setHeight(int $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getWidth(): int
+    {
+        return $this->width;
+    }
+
+    public function setWidth(int $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getGame(): Game
+    {
+        return $this->game;
+    }
+
+    public function setGame(Game $game): self
+    {
+        $this->game = $game;
+
+        return $this;
+    }
+}
