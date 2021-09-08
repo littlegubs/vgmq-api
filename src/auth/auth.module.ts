a55a9bd35d8fba31207f5bb9d85a887ca79ac2c9
Alexis Leroy <a.leroy@gamingprive.com>
2021-09-08 14:09:09 +0200

    fix unique validator & rename variable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,12 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 
+import { UserExistsRule } from '../users/unique.validator'
 import { UsersModule } from '../users/users.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { JwtRefreshTokenStrategy } from './strategies/jwt-refresh-token.strategy'
 import { JwtStrategy } from './strategies/jwt.strategy'
-import {UsersService} from "../users/users.service";
 
 @Module({
     controllers: [AuthController],
@@ -27,7 +27,7 @@ import {UsersService} from "../users/users.service";
         }),
         UsersModule,
     ],
-    providers: [AuthService, ConfigService, JwtStrategy, JwtRefreshTokenStrategy],
-    exports: [AuthService],
+    providers: [AuthService, ConfigService, JwtStrategy, JwtRefreshTokenStrategy, UserExistsRule],
+    exports: [AuthService, UserExistsRule],
 })
 export class AuthModule {}
