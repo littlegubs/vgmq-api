f2ade23db492002a506f206cf28284d9cd4b43c3
Alexis Leroy <a.leroy@gamingprive.com>
2022-04-20 23:25:58 +0200

    fix mp3 cutter + submit music buffer with socket.io

--- src/lobbies/dto/lobby-create.dto.ts
+++ src/lobbies/dto/lobby-create.dto.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer'
-import { IsNotEmpty } from 'class-validator'
+import {IsNotEmpty, IsNumber, Max, Min} from 'class-validator'
 
 export class LobbyCreateDto {
     @IsNotEmpty()
