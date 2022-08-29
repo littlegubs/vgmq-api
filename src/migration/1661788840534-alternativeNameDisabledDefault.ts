9a8e4a7fa2e6e3f60c237f594e3af219b408a59f
Alexis Leroy <alexis-60@hotmail.fr>
2022-08-29 18:03:39 +0200

    alternative disabled by default

---
+++ src/migration/1661788840534-alternativeNameDisabledDefault.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class alternativeNameDisabledDefault1661788840534 implements MigrationInterface {
+    name = 'alternativeNameDisabledDefault1661788840534'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`alternative_name\` CHANGE \`enabled\` \`enabled\` tinyint NOT NULL DEFAULT 0`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`alternative_name\` CHANGE \`enabled\` \`enabled\` tinyint NOT NULL DEFAULT '1'`);
+    }
+
+}
