d0ab43bc0100bc6397e4a482d4e9545a116afdd5
Alexis Leroy <alexis-60@hotmail.fr>
2022-09-23 17:10:45 +0200

    disconnect user after 30sec of lost connection

--- src/auth/strategies/ws.strategy.ts
+++ src/auth/strategies/ws.strategy.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
+import { InjectRepository } from '@nestjs/typeorm'
 import { Strategy } from 'passport-jwt'
 import { Socket } from 'socket.io'
+import { Repository } from 'typeorm'
 
+import { LobbyUser } from '../../lobbies/entities/lobby-user.entity'
+import { User } from '../../users/user.entity'
 import { UsersService } from '../../users/users.service'
 
 @Injectable()
 export class WsStrategy extends PassportStrategy(Strategy, 'ws') {
-    constructor(private usersService: UsersService) {
+    constructor(
+        private usersService: UsersService,
+        @InjectRepository(LobbyUser)
+        private lobbyUserRepository: Repository<LobbyUser>,
+    ) {
         super({
             jwtFromRequest: (req: Socket): string | null => {
                 const token = req.handshake.auth.token
@@ -19,7 +27,23 @@ export class WsStrategy extends PassportStrategy(Strategy, 'ws') {
         })
     }
 
-    async validate(payload: { username: string }) {
-        return this.usersService.findByUsername(payload.username)
+    async validate(payload: { username: string }): Promise<User | null> {
+        const user = await this.usersService.findByUsername(payload.username)
+        if (user !== null) {
+            const lobbyUser = await this.lobbyUserRepository.findOne({
+                relations: {
+                    user: true,
+                },
+                where: {
+                    user: {
+                        id: user.id,
+                    },
+                },
+            })
+            if (lobbyUser !== null) {
+                await this.lobbyUserRepository.save({ ...lobbyUser, toDisconnect: false })
+            }
+        }
+        return user
     }
 }
