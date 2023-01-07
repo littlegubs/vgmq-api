bf4ba1c2001ad534f9b576985b895b33f7d418f2
Alexis Leroy <alexis-60@hotmail.fr>
2023-01-07 19:16:17 +0100

    fetch less similar games

--- src/games/game.processor.ts
+++ src/games/game.processor.ts
@@ -40,7 +40,9 @@ export class GameProcessor {
             let similarGames: Game[] = []
             if (igdbGame.similar_games) {
                 for (const igdbSimilarGame of igdbGame.similar_games) {
-                    const similarGame = await this.igdbService.getSimilarGame(igdbSimilarGame)
+                    const similarGame = await this.gamesRepository.findOne({
+                        where: { igdbId: igdbSimilarGame.id },
+                    })
                     if (similarGame) similarGames = [...similarGames, similarGame]
                 }
             }
