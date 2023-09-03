53f86fb74dc814ff8154af1a4ffe02bb49a26b5e
Alexis Leroy <30618378+littlegubs@users.noreply.github.com>
2023-09-03 21:32:44 +0200

    Update lobby.gateway.ts

--- src/lobbies/lobby.gateway.ts
+++ src/lobbies/lobby.gateway.ts
@@ -438,7 +438,7 @@ export class LobbyGateway implements NestGateway, OnGatewayConnection {
 
         if (lobbyUser.lobby.status === LobbyStatuses.Buffering) {
             if (await this.lobbyUserService.areAllUsersReadyToPlay(lobbyUser.lobby)) {
-                await this.lobbyQueue.add('playMusic', lobbyUser.lobby.code? {jobId:  `lobby${(lobbyUser.lobby.code}playMusic${(lobbyUser.lobby.currentLobbyMusicPosition}EveryoneReady`})
+                await this.lobbyQueue.add('playMusic', lobbyUser.lobby.code, {jobId:  `lobby${lobbyUser.lobby.code}playMusic${lobbyUser.lobby.currentLobbyMusicPosition}EveryoneReady`})
             }
         }
     }
