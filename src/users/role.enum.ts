186fa8648d19d14b5eec669e88f034af917c018c
Alexis Leroy <alexis-60@hotmail.fr>
2024-07-06 10:52:05 +0200

    refactor ban user

--- src/users/role.enum.ts
+++ src/users/role.enum.ts
@@ -1,21 +1,5 @@
 export enum Role {
     User = 'user',
     Admin = 'admin',
-    SuperAdmin = 'superAdmin',
-}
-
-export enum RoleAction {
-    Ban = 'ban',
-}
-
-export const RoleRules: { [key in Role]: { [key in RoleAction]: Role[] } } = {
-    [Role.User]: {
-        [RoleAction.Ban]: [],
-    },
-    [Role.Admin]: {
-        [RoleAction.Ban]: [Role.User],
-    },
-    [Role.SuperAdmin]: {
-        [RoleAction.Ban]: [Role.User, Role.Admin],
-    },
+    SuperAdmin = 'super_admin',
 }
