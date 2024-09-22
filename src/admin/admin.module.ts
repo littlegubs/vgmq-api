5a51b8d8b5cda6ce5c757c48184fb4f62085e240
Alexis Leroy <alexis-60@hotmail.fr>
2024-09-22 18:02:49 +0200

    add system controller

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,12 +1,22 @@
+import { BullModule } from '@nestjs/bull'
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { File } from '../entity/file.entity'
+import { LobbyMusic } from '../lobbies/entities/lobby-music.entity'
+import { LobbyUser } from '../lobbies/entities/lobby-user.entity'
+import { Lobby } from '../lobbies/entities/lobby.entity'
 import { User } from '../users/user.entity'
+import { SystemController } from './system.controller'
 import { UsersController } from './users.controller'
 
 @Module({
-    controllers: [UsersController],
-    imports: [TypeOrmModule.forFeature([File, User])],
+    controllers: [UsersController, SystemController],
+    imports: [
+        TypeOrmModule.forFeature([File, User, Lobby, LobbyMusic, LobbyUser]),
+        BullModule.registerQueue({
+            name: 'lobby',
+        }),
+    ],
 })
 export class AdminModule {}
