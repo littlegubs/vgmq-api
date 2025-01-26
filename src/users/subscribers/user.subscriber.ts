2287ab72c05ebdf20b1bcfb0fa046a6e68d954da
Alexis Leroy <alexis-60@hotmail.fr>
2025-01-26 12:39:49 +0100

    patreon tier 1 free access list

--- src/users/subscribers/user.subscriber.ts
+++ src/users/subscribers/user.subscriber.ts
@@ -1,15 +1,30 @@
-import { Connection, EntitySubscriberInterface, EventSubscriber, UpdateEvent } from 'typeorm'
+import { ConfigService } from '@nestjs/config'
+import { DataSource, EntitySubscriberInterface, EventSubscriber, UpdateEvent } from 'typeorm'
 
+import { Role } from '../role.enum'
 import { User } from '../user.entity'
 
 @EventSubscriber()
 export class UserSubscriber implements EntitySubscriberInterface<User> {
-    constructor(connection: Connection) {
+    constructor(connection: DataSource, private configService: ConfigService) {
         connection.subscribers.push(this)
     }
 
     listenTo(): typeof User {
         return User
+    }
+
+    afterLoad(entity: User): void {
+        if (
+            !!entity.patreonAccount?.premium ||
+            entity.roles?.some((role) => [Role.Admin, Role.SuperAdmin].includes(role as Role)) ||
+            this.configService
+                .get<string>('PATREON_TIER_1_FREE_ACCESS')
+                ?.split(',')
+                .includes(String(entity.id))
+        ) {
+            entity.premium = true
+        }
     }
 
     async beforeUpdate(event: UpdateEvent<User>): Promise<void> {
