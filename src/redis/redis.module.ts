a44473398e9ad729f59d1a931cd861c6ac5f3910
Florian Robert <florian.robert@ekino.com>
2025-01-26 15:57:58 +0100

    add redis global module

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,31 @@
+import { Module, Global, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { createClient } from 'redis'
+
+@Global()
+@Module({
+    providers: [
+        {
+            provide: 'RedisClient',
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                const logger = new Logger('RedisModule')
+
+                const client = await createClient({
+                    url: `redis://${configService.get<string>(
+                        'REDIS_HOST',
+                    )}:${configService.get<string>('REDIS_PORT')}/${1}`,
+                })
+                    .on('error', (err) => {
+                        logger.error(err, 'Redis error')
+                    })
+                    .connect()
+                logger.log('Redis Connected')
+
+                return client
+            },
+        },
+    ],
+    exports: ['RedisClient'],
+})
+export class RedisModule {}
