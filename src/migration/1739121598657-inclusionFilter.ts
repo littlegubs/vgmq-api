ed7a576f862faecb3bcffa78a913a8a9cb6f21d7
Alexis Leroy <alexis-60@hotmail.fr>
2025-02-09 20:50:32 +0100

    include filter & optimize lobby filter

---
+++ src/migration/1739121598657-inclusionFilter.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InclusionFilter1739121598657 implements MigrationInterface {
+    name = 'InclusionFilter1739121598657'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`lobby_collection_filter\` CHANGE \`type\` \`type\` enum ('exclusion', 'limitation', 'inclusion') NOT NULL DEFAULT 'exclusion'`);
+        await queryRunner.query(`ALTER TABLE \`lobby_genre_filter\` CHANGE \`type\` \`type\` enum ('exclusion', 'limitation', 'inclusion') NOT NULL DEFAULT 'exclusion'`);
+        await queryRunner.query(`ALTER TABLE \`lobby_theme_filter\` CHANGE \`type\` \`type\` enum ('exclusion', 'limitation', 'inclusion') NOT NULL DEFAULT 'exclusion'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`lobby_theme_filter\` CHANGE \`type\` \`type\` enum ('exclusion', 'limitation') NOT NULL DEFAULT 'exclusion'`);
+        await queryRunner.query(`ALTER TABLE \`lobby_genre_filter\` CHANGE \`type\` \`type\` enum ('exclusion', 'limitation') NOT NULL DEFAULT 'exclusion'`);
+        await queryRunner.query(`ALTER TABLE \`lobby_collection_filter\` CHANGE \`type\` \`type\` enum ('exclusion', 'limitation') NOT NULL DEFAULT 'exclusion'`);
+    }
+
+}
