d2284d5b6a9b174aaf6290c2ddfd02b4df048d52
Alexis Leroy <30618378+littlegubs@users.noreply.github.com>
2025-05-08 17:42:15 +0200

    fix: made sure 'playMusic' processor isn't played twice
    Buffering users could rarely call the 'playMusic' processor at the same time, leading to a lobby crash

--- src/lobbies/lobby.gateway.ts
+++ src/lobbies/lobby.gateway.ts
@@ -445,7 +445,7 @@ export class LobbyGateway implements NestGateway, OnGatewayConnection {
         if (lobbyUser.lobby.status === LobbyStatuses.Buffering) {
             if (await this.lobbyUserService.areAllUsersReadyToPlay(lobbyUser.lobby)) {
                 await this.lobbyQueue.add('playMusic', lobbyUser.lobby.code, {
-                    jobId: `lobby${lobbyUser.lobby.code}playMusicEveryoneReady-${Date.now()}`,
+                    jobId: `lobby${lobbyUser.lobby.code}playMusicEveryoneReady`,
                 })
             }
         }
