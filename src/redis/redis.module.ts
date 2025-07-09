ff6eab995a3f5849da558a4fde51302cb46c2c0f
Florian Robert <florian.robert@ekino.com>
2025-07-09 20:07:03 +0200

    fix(redis): add password on redis connect

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -12,9 +12,7 @@ import { createClient } from 'redis'
                 const logger = new Logger('RedisModule')
 
                 const client = await createClient({
-                    url: `redis://${configService.get<string>(
-                        'REDIS_HOST',
-                    )}:${configService.get<string>('REDIS_PORT')}/${1}`,
+                    url: `redis://:${configService.get<string>('REDIS_PASSWORD')}@${configService.get<string>('REDIS_HOST')}:${configService.get<string>('REDIS_PORT')}/1`,
                 })
                     .on('error', (err) => {
                         logger.error(err, 'Redis error')
