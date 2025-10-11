94987479f003129fa76fdec67192e49a5d32baaa
Alexis Leroy <alexis-60@hotmail.fr>
2025-10-11 14:55:03 +0200

    chore: add loggers for IGDB webhooks

--- src/games/igdb-webhook.processor.ts
+++ src/games/igdb-webhook.processor.ts
@@ -1,5 +1,4 @@
 import { Process, Processor } from '@nestjs/bull'
-import { ElasticsearchService } from '@nestjs/elasticsearch'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Job } from 'bull'
 import { Repository } from 'typeorm'
@@ -8,52 +7,58 @@ import { User } from '../users/user.entity'
 import { Game } from './entity/game.entity'
 import { IgdbHttpService } from './http/igdb.http.service'
 import { IgdbGame } from './igdb.type'
-import { GamesService } from './services/games.service'
 import { IgdbService } from './services/igdb.service'
+import { Logger } from '@nestjs/common'
 
 @Processor('igdbWebhook')
 export class IgdbWebhookProcessor {
     constructor(
-        private gamesService: GamesService,
         private igdbService: IgdbService,
-        @InjectRepository(Game)
-        private gamesRepository: Repository<Game>,
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-        private elasticsearchService: ElasticsearchService,
-        private igdbHttpService: IgdbHttpService,
+        @InjectRepository(Game) private gamesRepository: Repository<Game>,
+        @InjectRepository(User) private igdbHttpService: IgdbHttpService,
     ) {}
+    private readonly logger = new Logger(IgdbWebhookProcessor.name)
 
     @Process('gameUpdate')
     async gameUpdate(job: Job<IgdbGame>): Promise<void> {
-        const game = await this.gamesRepository.findOne({
-            where: {
-                igdbId: job.data.id,
-                enabled: true,
-            },
-        })
-        if (game !== null) {
-            const [igdbGame] = await this.igdbHttpService.getDataFromUrl(job.data.url)
+        try {
+            this.logger.debug(`WEBHOOK: update game ${job.id}`)
+            const game = await this.gamesRepository.findOne({
+                where: {
+                    igdbId: job.data.id,
+                    enabled: true,
+                },
+            })
+            if (game !== null) {
+                const [igdbGame] = await this.igdbHttpService.getDataFromUrl(job.data.url)
 
-            if (!igdbGame) return
-            await this.igdbService.import(igdbGame)
+                if (!igdbGame) return
+                await this.igdbService.import(igdbGame)
+            }
+        } catch (error) {
+            this.logger.error(`WEBHOOK: An error occurred while updating game ${job.id}`, error)
         }
     }
 
     @Process('gameRemove')
     async gameRemove(job: Job<IgdbGame>): Promise<void> {
-        // get a game without music, so we don't delete a game with musics
-        const game = await this.gamesRepository
-            .createQueryBuilder('game')
-            .leftJoin('game.musics', 'music')
-            .andWhere('music.id IS NULL')
-            .andWhere('game.igdbId = :igdbId', { igdbId: job.data.id })
-            .getOne()
+        try {
+            this.logger.debug(`WEBHOOK: remove game ${job.id}`)
+            // get a game without music, so we don't delete a game with musics
+            const game = await this.gamesRepository
+                .createQueryBuilder('game')
+                .leftJoin('game.musics', 'music')
+                .andWhere('music.id IS NULL')
+                .andWhere('game.igdbId = :igdbId', { igdbId: job.data.id })
+                .getOne()
 
-        if (game === null) {
+            if (game === null) {
+                return
+            }
+            await this.gamesRepository.remove(game)
             return
+        } catch (error) {
+            this.logger.error(`WEBHOOK: An error occurred while deleting game ${job.id}`, error)
         }
-        await this.gamesRepository.remove(game)
-        return
     }
 }
